f4f864d07e6fa0017f965b008310625cb87de31b
mkoltsov <root@javabean.ru>
2015-02-23 15:44:46 +0100

    understanding es6, the basic

--- objects.js
+++ objects.js
@@ -4,3 +4,20 @@ let createPerson = (name,age) => ({name:name, age:age})
 //let createPerson1 = function(name, age){ return {name, age};}
 console.log(createPerson("chef",18))
 //console.log(createPerson1("chef",18))
+//doesn't work on node 0.12
+// var person = {
+// 	name:Nicholas,
+// 	sayName(){
+// 		console.log(this.name)
+// 	}
+// }
+
+var suffix = name
+//doesn't work on node 0.12
+// let person = {
+// 	["first" + suffix] : "Nicholas",
+// 	["lasr" + suffix] : "Zakas"
+// };
+
+// console.log(person["first name"])
+// console.log(person["last name"])
