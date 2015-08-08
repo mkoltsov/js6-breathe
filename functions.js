86996b86b7a63cf416cd0775c2381c99d09ed9dd
mkoltsov <root@javabean.ru>
2015-08-09 00:00:43 +0200

    understanding es6, the basic

--- functions.js
+++ functions.js
@@ -1,4 +1,11 @@
+"use strict";
 //default params don't work on node 0.12
 //(function def(chef=111, pupa=222) {
 //	console.log(chef, pupa)
 //})()
+//rest params don't work on node 0.12
+//function restParams(chef, ...rest) {
+//	console.log(chef, rest)
+//}
+
+//restParams(1,2,3,4,5,6,7,8)
