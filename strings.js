38db5aadbaf4b57627097d3e9b6cf0de657da16d
mkoltsov <root@javabean.ru>
2015-08-23 23:06:42 +0200

    understanding es6, the basics

---
+++ strings.js
@@ -0,0 +1,14 @@
+var ab = "chef has pupa"
+var ab1 = "1chef has pupa"
+var ab2 = "chef has pupa1"
+var ab3 = "1chef has pupa1"
+
+console.log(ab.startsWith('chef'))
+console.log(ab1.endsWith('pupa'))
+console.log(ab2.endsWith('pupa'))
+console.log(ab3.endsWith('pupa'))
+//doesn't work on node 0.12
+//console.log(ab3.includes("pupa"))
+var x = "x"
+console.log(x = x.repeat(10))
+console.log(x.length)
