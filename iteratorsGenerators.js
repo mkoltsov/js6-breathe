6550ea01c1f7f479c950b3deb9163ac1ac54215b
mkoltsov <root@javabean.ru>
2015-08-28 09:11:43 +0200

    understanding es6, the basic[A

--- iteratorsGenerators.js
+++ iteratorsGenerators.js
@@ -11,3 +11,46 @@ let iterator = createIterator()
 for (let i of iterator) {
 	console.log(i)
 }
+
+let createIterator1 = function *(items) {
+	for (let i=0; i< items.length;i++) {
+		yield items[i]
+	}
+}
+
+let iter = createIterator([1,2,3])
+
+console.log(iter.next())
+console.log(iter.next())
+console.log(iter.next())
+console.log(iter.next())
+console.log(iter.next())
+console.log(iter.next())
+
+let values = [1,2,3]
+
+let it = values[Symbol.iterator]();
+
+console.log(it.next())
+console.log(it.next())
+console.log(it.next())
+console.log(it.next())
+console.log(it.next())
+console.log(it.next())
+
+function isIterable(object) {
+	return typeof object[Symbol.iterator] === "function";
+}
+
+console.log(isIterable([1,2,3]))
+console.log(isIterable(1))
+console.log(isIterable("Chef"))
+console.log(isIterable(new Map()))
+console.log(isIterable(new Set()))
+
+let ctr = "CHEF"
+let iterat = ctr[Symbol.iterator]();
+
+for (let i of ctr) {
+	(() => console.log(i))()
+}
